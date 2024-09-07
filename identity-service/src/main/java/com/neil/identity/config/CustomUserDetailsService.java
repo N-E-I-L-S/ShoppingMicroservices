754ddb3c87aba7e5713ad6b9cce4b229dbe1c166
Neil Shukla <neilpshukla221203@gmail.com>
2024-09-07 17:39:21 +0530

    Security service added

---
+++ identity-service/src/main/java/com/neil/identity/config/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.neil.identity.config;
+
+import com.neil.identity.entity.User;
+import com.neil.identity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(username);
+        return user.map(CustomUserDetails::new).orElseThrow(() -> new UsernameNotFoundException("user not found with name :" + username));
+    }
+}
---
+++ identity-service/src/main/java/com/neil/identity/entity/User.java
@@ -0,0 +1,20 @@
+package com.neil.identity.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+}
