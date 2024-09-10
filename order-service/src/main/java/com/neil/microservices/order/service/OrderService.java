ae31f37011df36f7ac20229f764b4b7695107333
Neil Shukla <neilpshukla221203@gmail.com>
2024-09-10 09:26:21 +0530

    Kafka producer attached to order-service. notification-service is declared

--- order-service/src/main/java/com/neil/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/neil/microservices/order/service/OrderService.java
@@ -2,9 +2,13 @@ package com.neil.microservices.order.service;
 
 import com.neil.microservices.order.client.InventoryClient;
 import com.neil.microservices.order.dto.OrderRequest;
+import com.neil.microservices.order.event.OrderPlacedEvent;
 import com.neil.microservices.order.model.Order;
 import com.neil.microservices.order.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +16,20 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     @Autowired
     private InventoryClient inventoryClient;
 
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
 
     public void placeOrder(OrderRequest orderRequest){
-        boolean productInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        if(productInStock) {
+//        boolean productInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+//        if(productInStock) {
             Order order = new Order();
 
             order.setOrderNumber(UUID.randomUUID().toString());
@@ -29,9 +37,14 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
-        }
-        else{
-            throw  new RuntimeException("Product with skuCode "+ orderRequest.skuCode()+" is not in stock");
-        }
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(orderRequest.userDetails().email(),order.getOrderNumber());
+            System.out.println(orderPlacedEvent);
+            kafkaTemplate.send("order_placed", orderPlacedEvent);
+            log.info("Order {} sent to kafka topic order_placed", orderPlacedEvent);
+//        }
+//        else{
+//            throw  new RuntimeException("Product with skuCode "+ orderRequest.skuCode()+" is not in stock");
+//        }
     }
 }
